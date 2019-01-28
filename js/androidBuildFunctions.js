78f2b384536a17cd6f79dc8d17d56a738d8e8f7b
Michael David Kuckuk <Michael-David.Kuckuk@adesso.de>
2019-01-28 13:48:54 +0100

    Complete the build script

--- js/androidBuildFunctions.js
+++ js/androidBuildFunctions.js
@@ -1,6 +1,7 @@
 import util from 'util';
 import child_process, { spawn } from 'child_process';
 import Promise from 'core-js/es6/promise';
+import chalk from 'chalk';
 const exec = util.promisify(child_process.exec);
 
 export const getDeviceId = async () => {
@@ -11,14 +12,14 @@ export const getDeviceId = async () => {
     let result = null;
 
     if(!Array.isArray(adbDevices)) {
-        console.warn('No devices found! Please make sure at least one android device is plugged in and has USB ' +
-        'debugging enabled.');
+        console.warn(chalk.red('No devices found! Please make sure at least one android device is plugged in and ' +
+        'has USB debugging enabled.'));
     } else if(adbDevices.length === 1) {
         result = adbDevices[0].split(/\s+/)[0];
         console.log('Found device with ID ' + result);
     } else {
-        console.warn('Too many device IDs found! Please choose one of the following and then re-run this command with' +
-        'the extra argument --deviceId [your chosen ID]\n');
+        console.warn(chalk.red('Too many device IDs found! Please choose one of the following and then re-run ' +
+        'this command with the extra argument --deviceId [your chosen ID]\n'));
         adbDevices.map(line => line.split(/\s+/)[0]).forEach((id, index) => console.log((index + 1) + '. ' + id));
     }
 
@@ -33,8 +34,8 @@ export const verifyDeviceIsAvailable = async (deviceId) => {
     if(deviceIsAvailable) {
         console.log('Found device with ID ' + deviceId);
     } else {
-        console.warn('Could not find device with ID ' + deviceId + '! Please make sure it is ' +
-        'connected to your computer via USB and has USB debugging enabled.');
+        console.warn(chalk.red('Could not find device with ID ' + deviceId + '! Please make sure it is ' +
+        'connected to your computer via USB and has USB debugging enabled.'));
     }
 
     return deviceIsAvailable;
@@ -42,41 +43,81 @@ export const verifyDeviceIsAvailable = async (deviceId) => {
 
 export const startBundler = () => {
     console.log('Starting bundler');
-    const bundlerProcess = spawn('node node_modules/react-native/local-cli/cli.js start');
+    const bundlerProcess = spawn('node', [
+        'node_modules/react-native/local-cli/cli.js',
+        'start'
+    ]);
+
+    bundlerProcess.stdout.setEncoding('utf8');
+    bundlerProcess.stderr.setEncoding('utf8');
     
     bundlerProcess.stdout.on('data', (data) => {
-        console.log('bundler', data);
+        console.log(data);
     });
     
     bundlerProcess.stderr.on('data', (data) => {
-        console.error('bundler', data);
+        console.error(chalk.red(data));
     });
     
     bundlerProcess.on('close', (code) => {
-        console.log('Bundler process exited with code ' + code);
+        let message = 'Bundler process exited with code ' + code;
+
+        if(code !== 0) {
+            message = chalk.red(message);
+        }
+
+        console.log(message);
     });
 
-    bundlerProcess.on('error', console.error);
+    bundlerProcess.on('error', error => console.error(chalk.red(error)));
 };
 
 export const buildApk = (deviceId) => new Promise((resolve, reject) => {
     console.log('Building APK...');
-    const buildProcess = spawn('node node_modules/react-native/local-cli/cli.js run-android --deviceId ' + deviceId);
+    const buildProcess = spawn('node', [
+        'node_modules/react-native/local-cli/cli.js',
+        'run-android',
+        '--deviceId',
+        deviceId,
+        '--no-packager'
+    ]);
+
+    buildProcess.stdout.setEncoding('utf8');
+    buildProcess.stderr.setEncoding('utf8');
+
+    let warningHasBeenPrinted = false;
+    const handleData = (data, loggingFunction) => {
+        if(data.indexOf('app/build/outputs/apk/app-debug.apk') > -1 && !warningHasBeenPrinted) {
+            console.warn(chalk.yellow('WARNING: There will be some error messages regarding the installation of ' +
+            'the APK. This Error is expected! If it is the only error, there is no need wo worry.'));
+            warningHasBeenPrinted = true;
+            data = chalk.yellow(data);
+        }
+        
+        loggingFunction(data);
+    };
     
     buildProcess.stdout.on('data', (data) => {
-        console.log(data);
+        handleData(data, console.log);
     });
     
     buildProcess.stderr.on('data', (data) => {
-        console.error(data);
+        handleData(data, console.error);
     });
     
     buildProcess.on('close', (code) => {
-        console.log('Build process exited with code ' + code);
-        resolve();
+        if(code === 0) {
+            console.log('Build process exited with code ' + code);
+            resolve();
+        } else {
+            console.log(chalk.red('Build process exited with code ' + code));
+            reject();
+        }
     });
 
-    buildProcess.on('error', console.error);
+    buildProcess.on('error', error => {
+        console.error(chalk.red(error));
+    });
 });
 
 export const installApk = async (deviceId) => {
@@ -85,12 +126,14 @@ export const installApk = async (deviceId) => {
     ' install android/app/build/outputs/apk/ar/debug/app-ar-debug.apk';
     
     try {
-        const { stdout } = await exec(command);
+        await exec(command);
+        console.log('Installation successful');
     } catch(e) {
         if(e.stderr.indexOf('INSTALL_FAILED_ALREADY_EXISTS') > -1) {
-            console.warn('Installation of the APK failed. Please uninstall the old APK and then run ' +
-            '`npm build-android -- -n`');
+            console.warn(chalk.red('Installation of the APK failed. Please uninstall the old APK and then run ' +
+            '`npm build-android -- -n`'));
         } else {
+            //Unknown error, which should be passed along to caller
             throw e;
         }
     }
@@ -110,15 +153,18 @@ export const run = async (program) => {
     }
 
     if(deviceId !== null) {
-        // startBundler();
-
-        if(!program.noBuild) {
-            await buildApk(deviceId);
-        } else {
-            console.log('Skipping build');
+        try {
+            if(!program.noBuild) {
+                await buildApk(deviceId);
+            } else {
+                console.log(chalk.yellow('Skipping build'));
+            }
+            
+            await installApk(deviceId);
+            startBundler();
+        } catch(e) {
+            console.error(e);
         }
-
-        await installApk(deviceId);
     }
 };
 
